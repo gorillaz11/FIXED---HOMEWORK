5c65c71e1f09bcaa3c9237552fc7a0bb50a2dbd1
gorillaz11 <102285005+gorillaz11@users.noreply.github.com>
2022-03-28 20:26:48 +0300

    Add files via upload

---
+++ callin-my-nigga.py
@@ -0,0 +1,25 @@
+product_name="toilet price"
+product_price=5.0
+delivery_cost=50
+print("###############")
+print(product_name,":",product_price)
+
+answer=input("do you want to buy?")
+
+
+if answer ==str( "yes".lower() or "y".lower()):
+    order_quantity=int(input("how many?"))
+    order_cost=order_quantity*product_price
+else:
+    print("no toilet paper for you")        
+    
+    print("----------")
+    print("order info")
+    print(product_name,"x",order_quantity,"=",order_cost)
+    
+    answer=input("do you want delivery? ")
+    if answer=="yes".lower() or "y".lower():
+        order_cost+=delivery_cost
+        print("order cost (including delivery)",order_cost)
+    else:
+        print("too bad:")
